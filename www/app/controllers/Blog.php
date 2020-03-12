8903dd9ff4311b07f7ca95a66a30223368507806
leetaehee <lastride25@naver.com>
2020-03-12 14:50:20 +0900

    ci - index.php 제거하기

--- www/app/controllers/Blog.php
+++ www/app/controllers/Blog.php
@@ -3,18 +3,15 @@ class Blog extends CI_Controller
 {
     public function index()
     {
+        // 모델 사용
+        $this->load->model('Blog_model');
+
         $data['todo_list'] = array('Clean House', 'Call Mom', 'Run Errands');
 
         $data['title'] = 'My Real Title';
         $data['heading'] = 'My Real Heading';
 
         $this->load->view('blogview', $data);
-
-        // 모델 사용
-        $this->load->model('Blog_model');
-        $this->Blog_model->get_last_ten_entries();
-        $this->Blog_model->insert_entry();
-        $this->Blog_model->update_entry();
     }
 
     public function comments()
