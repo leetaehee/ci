7ecf16af6d4160a9ab71ef36061484f6f7b129b8
leetaehee <lastride25@naver.com>
2020-04-13 14:26:49 +0900

    파일업로드 테스트 완료

--- www/app/controllers/Upload.php
+++ www/app/controllers/Upload.php
@@ -15,15 +15,15 @@ class Upload extends CI_Controller
 
     public function do_upload()
     {
-        $config['upload_path'] = base_url() . '/uploads/';
-        $config['allowed_types'] = 'gif|jpg|png';
+        $config['upload_path'] = './uploads/';
+        $config['allowed_types'] = 'gif|jpg|png|txt';
         $config['max_size'] = 100;
         $config['max_width'] = 1024;
         $config['max_height'] = 768;
 
         $this->load->library('upload', $config);
 
-        if (! $this->upload->do_upload()) {
+        if (! $this->upload->do_upload('userfile')) {
             $error = array('error' => $this->upload->display_errors());
 
             $this->load->view('upload_form', $error);
