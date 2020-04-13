470dbcef046ede00af9aef95c133db923e86e033
leetaehee <lastride25@naver.com>
2020-04-13 14:47:29 +0900

    $config['base_url'] 동적으로 변경

--- www/app/config/config.php
+++ www/app/config/config.php
@@ -23,7 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://ci.study';
+//$config['base_url'] = 'http://ci.study';
+$config['base_url'] = $_SERVER['REQUEST_SCHEME'] . '://' . $config['HTTP_HOST'];
 
 /*
 |--------------------------------------------------------------------------
